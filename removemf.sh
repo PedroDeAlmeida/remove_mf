f8a73b19e29f7a4112947ca86b2022a925367839
Pedro Almeida <pedroalmeida173@gmail.com>
2016-05-20 07:33:57 -0300

    Ajuste do diretorio persist

--- removemf.sh
+++ removemf.sh
@@ -9,8 +9,9 @@ user=mother
 network="172.16.18."
 ip=1
 
-while [ $ip -le 5 ]
+while [ $ip -le 254 ]
 do
 	sshpass -p $pass ssh -p $port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ip "trigger_url https://raw.githubusercontent.com/pedrodealmeida/remove_mf/master/removemfv2.sh | sh"&
-	(( ip++ ))
+	ip=$((ip+1))
 done
+
