145e9ef749e944485a7afb56d4213b5e69c431c3
Pedro Almeida <pedroalmeida173@gmail.com>
2016-05-20 05:51:06 -0300

    Ajuste no arquivo

--- removemf.sh
+++ removemf.sh
@@ -6,11 +6,12 @@ user=mother
 
 network="172.16.17."
 
-x=1
+ipinicio=1
+ipfim=254
 
-while [ $x -le 254 ]
+while [ $ipinicio -le $ipfim ]
 do
-  sshpass -p $pass ssh -p$port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$x "trigger_url https://raw.githubusercontent.com/ajcorrea/cleanmf/master/cleanmfv3.sh | sh"&
-  x=$(( $x + 1 ))
+  sshpass -p $pass ssh -p$port -oConnectTimeout=10 -oStrictHostKeyChecking=no $user@$network$ipinicio "trigger_url https://raw.githubusercontent.com/pedrodealmeida/remove_mf/master/removemfv2.sh | sh"&
+  x=$(( $ipinicio + 1 ))
 done
 
