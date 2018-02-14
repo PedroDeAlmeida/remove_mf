de992afb28d93811379891bc1b3879c3d9335a00
PedroDeAlmeida <pedroalmeida173@gmail.com>
2018-02-14 10:14:04 -0200

    Update removemfv2.sh

--- removemfv2.sh
+++ removemfv2.sh
@@ -1,31 +1,15 @@
 #!/bin/sh
 # Pedro Almeida - pedroalmeida173@gmail.com
 
-cd /etc/persistent/
-rm mf.tar
-rm -rf .mf
-rm -r mcuser
-rm rc.poststart
-
+rm /etc/persistent/mf.tar
+rm -rf /etc/persistent/.mf
+rm -r /etc/persistent/mcuser
+rm /etc/persistent/rc.poststart
 cat /etc/passwd | grep -v mcuser >> /etc/passwd2
-cat /etc/passwd2 >> /etc/passwd
+cat /etc/passwd2 > /etc/passwd
 rm /etc/passwd2
-
 cfgmtd -w -p /etc/
-
-versao=`cat /etc/version | cut -d'.' -f1`
-
-cd /tmp
-rm -rf /tmp/X*.bin
-
-if [ "$versao" == "XM" ]; then
-        URL='http://dl.ubnt.com/firmwares/XN-fw/v5.6.5/XM.v5.6.5.29033.160515.2119.bin'
-        wget -c $URL
-        ubntbox fwupdate.real -m /tmp/XM.v5.6.5.29033.160515.2119.bin
-else
-        URL='http://dl.ubnt.com/firmwares/XW-fw/v5.6.5/XW.v5.6.5.29033.160515.2108.bin'
-        wget -c $URL
-        ubntbox fwupdate.real -m /tmp/XW.v5.6.5.29033.160515.2108.bin
-fi
-
+killall -9 search
+killall -9 mother
+killall -9 sleep
 reboot
